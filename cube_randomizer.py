43a1d61652443030d0678f7bcf6d79f166a6256b
Assalat3 <34478752+Assalat3@users.noreply.github.com>
2018-03-11 18:04:44 +0200

    The randomizer written in python3
    11.3.2018, 18:04:30. Actually created a working loop. Whoops.

--- cube_randomizer.py
+++ cube_randomizer.py
@@ -7,9 +7,9 @@ moveset3x3 = ['R', '2R', 'L', '2L', 'U', '2U', 'D', '2D', 'B', '2B',
          "R'", "2R'", "L'", "2L'", "U'", "2U'", "D'","2D'", "B'", "2B'",
          "M", "2M", "M'", "2M'"]
 
-cube = raw_input("What cube are you solving? Valid answers: 2x2, 3x3, quit.")
-
 def randomizer():
+    
+    cube = raw_input("What cube are you solving? Valid answers: 2x2, 3x3, quit.")
     
     if cube == "quit":
         return
@@ -17,12 +17,15 @@ def randomizer():
     elif cube == "2x2":
         for i in range(11):
             print(moveset2x2[int(floor(uniform(0, len(moveset2x2))))])
+        randomizer()
             
     elif cube == "3x3":
         for i in range(20):
             print(moveset3x3[int(floor(uniform(0, len(moveset3x3))))])
+        randomizer()
             
     else:
         print("Invalid answer.")
+        randomizer()
             
 randomizer()

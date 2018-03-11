8764bcb64295feee2d0ef4dc779a67d873f27603
Assalat3 <34478752+Assalat3@users.noreply.github.com>
2018-03-11 17:40:03 +0200

    Add files via upload

--- cube_randomizer.py
+++ cube_randomizer.py
@@ -1,10 +1,25 @@
 import math
 import random
-moves = ['R', '2R', 'L', '2L', 'U', '2U', 'D', '2D', 'B', '2B',
+moveset2x2 = ['R', '2R', 'L', '2L', 'U', '2U', 'D', '2D', 'B', '2B',
          "R'", "2R'", "L'", "2L'", "U'", "2U'", "D'","2D'", "B'", "2B'"]
+moveset3x3 = ['R', '2R', 'L', '2L', 'U', '2U', 'D', '2D', 'B', '2B',
+         "R'", "2R'", "L'", "2L'", "U'", "2U'", "D'","2D'", "B'", "2B'",
+         "M", "2M", "M'", "2M'"]
 
-cube = raw_input("Which cube are you solving? Valid answers: 2x2, 3x3.")
+cube = raw_input("What cube are you solving? Valid answers: 2x2, 3x3, quit.")
 
-if cube == "2x2":
-    for i in range(11):
-        print(moves[int(math.floor(random.uniform(0, len(moves))))])
+def randomizer():
+    if cube == "quit":
+        return
+    elif cube == "2x2":
+        for i in range(11):
+            print(moveset2x2[int(math.floor(random.uniform(
+                0, len(moveset2x2))))])
+    elif cube == "3x3":
+        for i in range(20):
+            print(moveset3x3[int(math.floor(random.uniform(
+                0, len(moveset3x3))))])
+    else:
+        print("Invalid answer.")
+            
+randomizer()

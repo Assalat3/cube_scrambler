415f642dbd11d5a938fa678015d05824894dd450
Assalat3 <34478752+Assalat3@users.noreply.github.com>
2018-03-11 17:55:29 +0200

    Add files via upload
    Cleaned import and code depending on it.

--- cube_randomizer.py
+++ cube_randomizer.py
@@ -1,5 +1,6 @@
-import math
-import random
+from math import floor
+from random import uniform
+
 moveset2x2 = ['R', '2R', 'L', '2L', 'U', '2U', 'D', '2D', 'B', '2B',
          "R'", "2R'", "L'", "2L'", "U'", "2U'", "D'","2D'", "B'", "2B'"]
 moveset3x3 = ['R', '2R', 'L', '2L', 'U', '2U', 'D', '2D', 'B', '2B',
@@ -9,16 +10,18 @@ moveset3x3 = ['R', '2R', 'L', '2L', 'U', '2U', 'D', '2D', 'B', '2B',
 cube = raw_input("What cube are you solving? Valid answers: 2x2, 3x3, quit.")
 
 def randomizer():
+    
     if cube == "quit":
         return
+    
     elif cube == "2x2":
         for i in range(11):
-            print(moveset2x2[int(math.floor(random.uniform(
-                0, len(moveset2x2))))])
+            print(moveset2x2[int(floor(uniform(0, len(moveset2x2))))])
+            
     elif cube == "3x3":
         for i in range(20):
-            print(moveset3x3[int(math.floor(random.uniform(
-                0, len(moveset3x3))))])
+            print(moveset3x3[int(floor(uniform(0, len(moveset3x3))))])
+            
     else:
         print("Invalid answer.")
             

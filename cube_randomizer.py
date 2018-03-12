0a7227cceee7cf78bed386a1541c456b6111a52d
Assalat3 <34478752+Assalat3@users.noreply.github.com>
2018-03-12 19:08:39 +0200

    Add files via upload
    Added some documentation.
    Added a script that removes identical move sets appearing back to back.

--- cube_randomizer.py
+++ cube_randomizer.py
@@ -1,3 +1,4 @@
+#Importing the things that we need.
 from math import floor
 from random import uniform
 
@@ -14,7 +15,7 @@ moves3x3 = []
 #Function for randomizing things.
 def randomizer():
     
-    #What cube the user is solving.
+    #What cube the user is solving. Also the beginning of the "loop"
     cube = raw_input("What cube are you solving? Valid answers: 2x2, 3x3, quit  ")
     
     if cube == "quit" or cube == "q":
@@ -24,13 +25,25 @@ def randomizer():
         for i in range(11):
             
             #Add all the moves to the list in the correct type.
-            moves2x2.append(moveset2x2[int(floor(uniform(0, len(moveset2x2))))])
-              
-        #Prints the list with each moveset on a new line.
+            moves2x2.append(moveset2x2[int(floor(uniform(
+                0, len(moveset2x2))))])
+            
+        #Remove back-to-back moves that are identical. Add new move to the
+        #end of the list.
         n = 0
         while n <= 10:
-            print(moves2x2[n])
+            if moves2x2[n] == moves2x2[n - 1]:
+                moves2x2.pop(n)
+                moves2x2.append(moveset2x2[int(floor(uniform(
+                    0, len(moveset2x2))))])
+                n -= 1
             n += 1
+            
+        #Prints the list with each moveset on a new line.
+        m = 0
+        while m <= 10:
+            print(moves2x2[m])
+            m += 1
         
         #Clear the list for the next scramble.
         for i in range(len(moves2x2)):
@@ -42,7 +55,19 @@ def randomizer():
         for i in range(20):
             
             #Add all the moves to the list in the correct type.
-            moves3x3.append(moveset3x3[int(floor(uniform(0, len(moveset3x3))))])
+            moves3x3.append(moveset3x3[int(floor(uniform(
+                0, len(moveset3x3))))])
+            
+        #Remove back-to-back moves that are identical. Add new move to the
+        #end of the list.
+        n = 0
+        while n <=19:
+            if moves3x3[n] == moves3x3[n - 1]:
+                moves3x3.pop(n)
+                moves3x3.append(moveset3x3[int(floor(uniform(
+                    0, len(moveset3x3))))])
+                n -= 1
+            n += 1
             
         #Prints the list with each moveset on a new line.
         n = 0
@@ -51,7 +76,7 @@ def randomizer():
             n += 1
         
         #Clear the list for the next scramble.
-        for i in range(len(moves3x3)):
+        for i in range(20):
             moves3x3.pop()
             
         randomizer()

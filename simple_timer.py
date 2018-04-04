99e2964ea3839c025970535e773e40cd663d6265
Assalat3 <34478752+Assalat3@users.noreply.github.com>
2018-04-04 16:02:51 +0300

    Import less modules.

--- simple_timer.py
+++ simple_timer.py
@@ -1,15 +1,17 @@
-import time
+from time import time
 
 def timer():
     try:
         input("Press enter to start the timer")
     except SyntaxError:
         pass
-        t0 = time.time()
+        t0 = time()
         try:
             input("Press enter to stop the timer")
         except SyntaxError:
             pass
-            t1 = time.time()
-            tfinal = str(t1 - t0)
-            print("Your time was: " + tfinal)
+            t1 = time()
+            tfinal = (t1 - t0)
+            print("Your time was:  {0: .3f}").format(tfinal)
+
+timer()
